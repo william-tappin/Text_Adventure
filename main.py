5e774f87aaab91b1dfc2bf836d923b23c07e0b61
will <w1ll1am2728@outlook.com>
2025-05-15 20:02:06 +0100

    added a function to add text to output as if it was being spoken

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import tkinter as tk
+import time
 
-def get_input():
-    text = Output.get("1.0", tk.END)
-
-
-
-def clear_text():
-    Output.delete("1.0", tk.END)
+def GetInput():
+    text = Input.get("1.0", tk.END)
 
 
+
+def ClearText():
+    Input.delete("1.0", tk.END)
+
+CurrentIndex = 0
+def TypeText(text):
+    global CurrentIndex
+    if CurrentIndex < len(text):
+        Output.config(state=tk.NORMAL)
+        Output.insert(tk.END, text[CurrentIndex])
+        CurrentIndex += 1
+        MainWindow.after(100,lambda: TypeText(text))  # call again after 50ms
 
 
 MainWindow = tk.Tk()
@@ -20,4 +28,7 @@ Input = tk.Text(MainWindow, height = 25, width = 100, foreground = "white",backg
 
 Output.pack()
 Input.pack()
+
+TypeText(string)
 MainWindow.mainloop()
+
